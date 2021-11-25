d5cf659f8ba61c31b988cd51975321bf1fcb6d5f
Indra2108 <fizipratama21@gmail.com>
2021-11-25 14:35:11 +0700

    Add Home and Bookmark screen

---
+++ src/Bookmark/index.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+
+export default Bookmark = () => {
+    return(
+        <View>
+            
+        </View>
+    )
+}
---
+++ src/Home/index.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+
+export default Home = () => {
+    return(
+        <View>
+            
+        </View>
+    )
+}
