8fb4b92f2c6acc7324e002c11b00874781f2e162
Indra2108 <fizipratama21@gmail.com>
2021-11-25 20:09:10 +0700

    Add Search screen for searching  article

---
+++ src/Search/index.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+
+export default Content = () => {
+    return(
+        <View style={styles.container}>
+
+        </View>
+    )
+}
