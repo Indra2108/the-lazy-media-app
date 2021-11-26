84bd042f65992bd20e6476349e138949f162083b
Indra2108 <fizipratama21@gmail.com>
2021-11-26 07:37:24 +0700

    Add Article Card Add author.png Add date.png

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,26 +1,45 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { View, Text, Image, TouchableOpacity, ImageBackground } from 'react-native';
 
 // IMPORT STYLES
 import styles from './styles';
 
 // IMPORT IMAGES
 import lup from './assets/lup.png';
-import sample from './assets/sample.jpg'
+import sample from './assets/sample.jpg';
+import author from './assets/author.png';
+import date from './assets/date.png';
 
 export default Home = () => {
     return (
         <View style={styles.container}>
-            <View style={styles.headers}>
-                <TouchableOpacity style={styles.backgroundSearchs}>
-                    <Image source={lup} style={styles.lups} />
-                    <Text style={styles.textSearchs}>Cari Artikel</Text>
-                </TouchableOpacity>
+            {/* <View style={styles.headers}> */}
+            <TouchableOpacity style={styles.backgroundSearchs}>
+                <Image source={lup} style={styles.lups} />
+                <Text style={styles.textSearchs}>Cari Artikel</Text>
+            </TouchableOpacity>
+            {/* </View> */}
 
-                <TouchableOpacity style={styles.articleCard}>
-                    <Image />
-                </TouchableOpacity>
-            </View>
+            <TouchableOpacity style={styles.articleCards}>
+                <Image source={sample} style={styles.pictArticleCards} />
+
+                <View style={styles.backgroundTag}>
+                    <Text style={styles.teksTag}>news</Text>
+                </View>
+                <View style={styles.backgroundTitleArticle}>
+                    <Text style={styles.titleArticle}>Battlefield 2042 Menjadi Game Dengan Rating Terburuk!</Text>
+                </View>
+                <View style={styles.backgroundInfoArticle}>
+                    <View style={styles.infoArticle}>
+                        <Image source={author} style={styles.pictInfoArticle} />
+                        <Text style={styles.authorInfoArticle}>Aldy Wayong</Text>
+                    </View>
+                    <View style={styles.infoArticle}>
+                        <Image source={date} style={styles.pictInfoArticle} />
+                        <Text style={styles.dateInfoArticle}>November 19, 2021</Text>
+                    </View>
+                </View>
+            </TouchableOpacity>
         </View>
     )
 }
--- src/Home/styles.js
+++ src/Home/styles.js
@@ -6,15 +6,15 @@ export default StyleSheet.create({
         padding: 20,
         backgroundColor: '#1b1f27'
     },
-    headers: {
-        
-    },
+    // headers: {
+
+    // },
     backgroundSearchs: {
         backgroundColor: '#101319',
         flexDirection: 'row',
         padding: '5%',
         borderRadius: 10
-    }, 
+    },
     textSearchs: {
         color: '#4d5361'
     },
@@ -22,5 +22,61 @@ export default StyleSheet.create({
         width: '6.5%',
         height: '100%',
         marginRight: '5%'
+    },
+    articleCards: {
+        marginTop: '6%',
+        justifyContent: 'space-between'
+    },
+    backgroundTag: {
+        backgroundColor: '#fbad18',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        padding: 13,
+        borderTopStartRadius: 10,
+        borderBottomEndRadius: 10
+    },
+    teksTag: {
+        fontWeight: 'bold',
+        color: 'black'
+    },
+    pictArticleCards: {
+        borderRadius: 10,
+        width: '100%',
+    },
+    titleArticle: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 15
+    },
+    backgroundTitleArticle: {
+        position: 'absolute',
+        bottom: '19%',
+        padding: 7
+    },
+    backgroundInfoArticle: {
+        width: '100%',
+        position: 'absolute',
+        bottom: 0,
+        padding: 7,
+        flexDirection: 'row'
+    },
+    infoArticle: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    pictInfoArticle: {
+        width: '11%',
+        height: '100%',
+        marginRight: '5%'
+    },
+    authorInfoArticle: {
+        color: 'white',
+        marginRight: '5%',
+        fontSize: 10
+    },
+    dateInfoArticle: {
+        color: 'white',
+        fontSize: 10
     }
 })
